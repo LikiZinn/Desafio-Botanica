df1e2c4e5799c763072fca34422ca4036b635a16
Cristian Ferreira <122299900+LikiZinn@users.noreply.github.com>
2026-06-04 23:21:09 -0300

    Add files via upload

--- Desafio Botanica/JS/script.js
+++ Desafio Botanica/JS/script.js
@@ -1,1 +1,22 @@
+function filtrarProdutos() {
+    // 1. Pega o valor digitado e converte para minúsculas
+    let input = document.getElementById('barra_busca').value.toLowerCase();
+    
+    // 2. Pega todos os itens que você quer filtrar (ex: itens com classe 'produto-item')
+    let listaProdutos = document.getElementsByClassName('produto-item');
 
+    // 3. Loop por todos os itens
+    for (let i = 0; i < listaProdutos.length; i++) {
+        // Pega o texto do item (pode ser o título do produto)
+        let titulo = listaProdutos[i].textContent || listaProdutos[i].innerText;
+        
+        // 4. Verifica se o texto do item contém o que foi digitado
+        if (titulo.toLowerCase().indexOf(input) > -1) {
+            // Se sim, mostra o item
+            listaProdutos[i].style.display = "";
+        } else {
+            // Se não, esconde o item
+            listaProdutos[i].style.display = "none";
+        }
+    }
+}
